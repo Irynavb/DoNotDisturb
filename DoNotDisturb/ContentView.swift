717d13cc321178d87e0b6269c4da74882386240e
Irynavb <irynabetancourt@gmail.com>
2021-02-24 13:03:06 -0500

    initial commit

---
+++ DoNotDisturb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoNotDisturb
+//
+//  Created by Iryna V Betancourt on 2/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
